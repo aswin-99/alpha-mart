ed49f219f2cf253fbe00ae7ef807e076c34ccd8a
unknown <achuayappan07@gmail.com>
2022-06-19 08:53:35 +0530

    my project

--- api/index.js
+++ api/index.js
@@ -10,6 +10,8 @@ const cartRoute = require("./routes/cart");
 const orderRoute = require("./routes/order");
 const stripeRoute = require("./routes/stripe");
 const cors = require("cors");
+const path = require("path")
+
 
 
 mongoose
@@ -27,6 +29,11 @@ app.use("/api/products", productRoute);
 app.use("/api/carts", cartRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/checkout", stripeRoute);
+app.use(express.static(path.join(__dirname, "client", "build")))
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Backend server is running!");
